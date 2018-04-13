342cbea5b6b7c3ae43acd79f9878473bdbe48788
P3B3 <peanut_butter@tutanota.com>
2018-04-13 20:04:47 +0300

    add registration + logout :door:

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     path('', views.home),
-    path('login/', login, {'template_name': 'accounts/login.html'})
+    path('login/', login, {'template_name': 'accounts/login.html'}),
+    path('logout/', logout, {'template_name': 'accounts/logout.html'}),
+    path('register/', views.register, name='register')
 ]
--- sgn_project/urls.py
+++ sgn_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from sgn_project import views
 
 urlpatterns = [
+    path('', views.login_redirect, name='login_redirect'),
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
 ]
