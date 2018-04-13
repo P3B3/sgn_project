afa941eaea80d2ead38d6a91179791f28a889795
P3B3 <peanut_butter@tutanota.com>
2018-04-14 01:15:46 +0300

    add login required in middleware :zap:

--- sgn_project/settings.py
+++ sgn_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'sgn_project.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'sgn_project.urls'
@@ -126,6 +127,14 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/account/'
+
+LOGIN_URL = '/account/login/'
+
+LOGIN_EXEMPT_URLS = (
+        'account/logout/',
+        'account/register/'
+
+)
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
