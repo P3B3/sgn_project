d28299cd15dffd4622b861124f0c9e712116b6e2
P3B3 <peanut_butter@tutanota.com>
2018-04-14 11:09:36 +0300

    password reset fix :fire:

--- accounts/urls.py
+++ accounts/urls.py
@@ -19,10 +19,17 @@ urlpatterns = [
     path('profile/', views.view_profile, name='view_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('change-password/', views.change_password, name='change_password'),
+
     path('reset-password/', password_reset, {'template_name': 'accounts/reset_password.html',
-         'post_reset_redirect': 'accounts:password_reset_done'}, name='reset_password'),
-    path('reset-password/done/', password_reset_done, name='password_reset_done'),
+         'post_reset_redirect': 'accounts:password_reset_done',
+            'email_template_name': 'accounts/reset_password_email.html'}, name='reset_password'),
+
+    path('reset-password/done/', password_reset_done, {'template_name': 'accounts/reset_password_done'},
+         name='password_reset_done'),
+
     path('reset-password/confirm/<uidb64>/<token>/',
-         password_reset_confirm, name='password_reset_confirm'),
+         password_reset_confirm, {'post_reset_redirect': 'accounts:password_reset_complete'},
+         name='password_reset_confirm'),
+
     path('reset-password/complete/', password_reset_complete, name='password_reset_complete')
 ]
