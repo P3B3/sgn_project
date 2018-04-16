c49bdf057c030fdd15ea361cea3cd0ff08ff7c30
P3B3 <peanut_butter@tutanota.com>
2018-04-17 00:42:44 +0300

    add home app + view :house:

--- home/views.py
+++ home/views.py
@@ -1,5 +1,22 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from home.forms import HomeForm
 
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+    def get(self, request):
+        form = HomeForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = HomeForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data['post']
+            # return redirect('home:home')
+
+        args = {'form': form, 'text': text}
+        return render(request, self.template_name, args)
+
+
